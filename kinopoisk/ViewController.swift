8575e88ad31235d1386d4870912dc12e851ca278
ferentsalena <let4itsa@gmail.com>
2023-08-18 15:10:30 +0300

    init

---
+++ kinopoisk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kinopoisk
+//
+//  Created by lifetech on 18.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
